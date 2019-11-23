2a82d169cbdc5377cff7b5460e2a728a5e24b0a9
Robert <gusroberth@student.gu.se>
2019-11-24 00:47:38 +0100

    cleaning up

--- GRUNetwork.py
+++ GRUNetwork.py
@@ -1,11 +1,7 @@
 from torch import nn
 import torch
 
-<<<<<<< HEAD
 class RNN_GRU(nn.Module):
-=======
-class GRUNet(nn.Module):
->>>>>>> 7c46f5104ff2c97d1d276d29b36a8c19677b72a9
     def __init__(self, vocab_size, seq_len, input_size, hidden_size, num_layers, output_size, device, dropout=0.0):
         super().__init__()
         self.num_layers = num_layers
@@ -18,7 +14,6 @@ class GRUNet(nn.Module):
                           num_layers=self.num_layers, batch_first=True, dropout=dropout).to(device)
         self.fc = nn.Linear(hidden_size * seq_len, output_size).to(device)
 
-<<<<<<< HEAD
     def forward(self, sequence, hidden_layer, device):
         output = self.emb(sequence).to(device)
         hidden_layer = hidden_layer.to(self.device)
@@ -26,20 +21,15 @@ class GRUNet(nn.Module):
         output = output.contiguous().view(-1, self.hidden_size *
                                           len(sequence[0]))
         output = self.fc(output).to(device)
-        return output, hidden_layer
-=======
+
+return output, hidden_layer
     def forward(self, sequence, hidden_layer):
         output = self.emb(sequence)
         output, hidden_layer = self.gru(output, hidden_layer)
         output = output.contiguous().view(-1, self.hidden_size *
                                           len(sequence[0]))
         output = self.fc(output)
->>>>>>> 7c46f5104ff2c97d1d276d29b36a8c19677b72a9
-
         return output, hidden_layer
+
     def init_hidden(self, batch_size):
         return torch.zeros(self.num_layers, batch_size, self.hidden_size).float().to(self.device)
-<<<<<<< HEAD
-=======
-
->>>>>>> 7c46f5104ff2c97d1d276d29b36a8c19677b72a9
