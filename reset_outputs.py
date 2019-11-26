d615d4eed2dfa9ab7b3af11d41d2e8c73f17112a
Rob <gusroberth@student.gu.se>
2019-11-26 15:56:38 +0100

    reset all outpus

---
+++ reset_outputs.py
@@ -0,0 +1,10 @@
+import os 
+
+def delete_everything(list_of_paths):
+    for i in list_of_paths:
+        files = os.listdir(i)
+        for a in files:
+            os.remove(i+a)
+            
+            
+delete_everything(['dataloaders/', 'trained_models/', 'vocab/', 'config/'])
