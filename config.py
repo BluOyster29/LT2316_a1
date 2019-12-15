321bc6935550b465ddf1ebec01352f89768ddd72
Rob <gusroberth@student.gu.se>
2019-12-15 11:39:58 +0100

    updates

--- config.py
+++ config.py
@@ -1,18 +1,28 @@
 import torch, json, os
 
-CONFIG = {'vocab_size'     : int(),
-         'sequence_length' : 100,
-         'batch_size'      : 1,
-         'input_size'      : 100,
-         'hidden_size'     : 256,
-         'number_layers'   : 2,
-         'output_size'     : 10,
-         'device'          : 'cuda:01'
-         }
-
-def update_config(config_file):
-
+def gen_config(path):
+    
+    EMPTY_CONFIG = {'vocab_size'     : int(),
+               'sequence_length' : 100,
+               'batch_size'      : 1,
+               'input_size'      : 100,
+               'hidden_size'     : 256,
+               'number_layers'   : 2,
+               'output_size'     : 10,
+               'device'          : str(),
+               'languages'       : list()
+                   }
+    
     with open('config/config.json', 'w') as outfile:
-        json.dump(CONFIG, outfile, sort_keys=True, indent=3)
+        json.dump(EMPTY_CONFIG, outfile, sort_keys=True, indent=3)
+    return EMPTY_CONFIG
+    
+def update_config(config_file):
+    with open('config/config.json', 'w') as outfile:
+        json.dump(config_file, outfile, sort_keys=True, indent=3)
 
 
+def get_config(config_file):
+    with open(config_file) as json_file:
+        config = json.load(json_file)
+    return config   
